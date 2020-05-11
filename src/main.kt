e59f9e6fed605fff57023b3ce75ca9ffca76fb4a
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-11 10:15:53 -0300

    cap 3 completo, introducao a orientacao a objetos

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,60 @@
 fun main() {
     println("Bem vindo ao Bytebank");
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 100.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println("Titular: ${contaAlex.titular}")
+    println("Número.: ${contaAlex.numero}")
+    println("Saldo..: ${contaAlex.saldo}")
+
+    println("Titular: ${contaFran.titular}")
+    println("Número.: ${contaFran.numero}")
+    println("Saldo..: ${contaFran.saldo}")
+
+    deposita(contaAlex,50.0)
+    println("Saldo..: ${contaAlex.saldo}")
+    contaFran.saldo += 70.0
+
+    println("Deponsitando na conta da Fran..: ${contaAlex.saldo}")
+    deposita(contaFran, valor = 70.0)
+    println("Saldo..: ${contaFran.saldo}")
 
 }
 
-private fun lacoDeRepeticao() {
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
+private fun testaCopiasReferencia() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+//    println("numeroX $numeroX")
+//    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "Joao"
+    var contaMaria = contaJoao
+    contaMaria.titular = "Maria"
+
+    println("titular conta joao: $contaJoao")
+    println("titular conta maria: $contaMaria")
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+private fun testaLacos() {
     var i = 0
     while (i < 5) {
         val titular = "Gabriel $i"
