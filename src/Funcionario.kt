49f72b3e1fe4e754050c2574d937338d06598aa0
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-11 14:15:23 -0300

    modulo 02 aula 01 - atividade 6 completo- criacao da classe funcionario

---
+++ src/Funcionario.kt
@@ -0,0 +1,10 @@
+class Funcionario(
+    val nome: String,
+    val cpf: String,
+    val salario: Double
+) {
+    fun bonificacao(): Double {
+        return this.salario * 0.1
+    }
+}
+
--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,17 @@
 fun main() {
     println("Bem vindo ao Bytebank");
+
+    val funcionario = Funcionario(
+        nome = "Gabriel",
+        cpf = "39562057828",
+        salario = 1000.0
+    )
+
+    println("Nome: ${funcionario.nome}")
+    println("CPF: ${funcionario.cpf}")
+    println("Salário: ${funcionario.salario}")
+
+    println("bonificação ${funcionario.bonificacao()}")
 }
 
 
