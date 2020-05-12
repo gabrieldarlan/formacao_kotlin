1a55007e4042da7803fe9283b624dd8023ea7a2c
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-12 07:23:16 -0300

    cap 1 completo

--- src/main.kt
+++ src/main.kt
@@ -4,8 +4,7 @@ fun main() {
     val funcionario = Funcionario(
         nome = "Gabriel",
         cpf = "39562057828",
-        salario = 1000.0,
-        tipo = 1
+        salario = 1000.0
     )
 
     println("Nome: ${funcionario.nome}")
@@ -13,6 +12,24 @@ fun main() {
     println("Salário: ${funcionario.salario}")
 
     println("bonificação ${funcionario.bonificacao()}")
+
+    var gerente = Gerente(
+        nome = "Jaqueline",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 111
+    )
+    println()
+    println("Nome: ${gerente.nome}")
+    println("CPF: ${gerente.cpf}")
+    println("Salário: ${gerente.salario}")
+    println("Gerente bonificação ${gerente.bonificacao()}")
+
+    if (gerente.autenticacao(senha = 111)) {
+        println("Autenticou com sucesso")
+    } else {
+        println("falha na autenticacao")
+    }
 }
 
 
